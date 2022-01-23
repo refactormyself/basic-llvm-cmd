cc7fe89bead777a0bda011c26262177d9e9c8428
SAHEED <refactormyself@gmail.com>
2022-01-23 14:36:36 +0100

    FIXED something

---
+++ include/common.hpp
@@ -0,0 +1,6 @@
+#ifndef KAL_COMPILER_COMMON_HPP
+#define KAL_COMPILER_COMMON_HPP
+
+bool HandleArgs(const std::string& inFilename, const std::string& outFilename);
+
+#endif //KAL_COMPILER_COMMON_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "llvm/Support/CommandLine.h"
 #include <fstream>
 
+#include "common.hpp"
+
 // #include "llvm/Support/VirtualFileSystem.h"
 
 using namespace llvm;
 using namespace std;
 
-bool InteractShellMode = false;
 
 // -o <filename>
 cl::opt<string> OutputFilename("o", cl::desc("Specify output filename"), cl::value_desc("filename"));
@@ -16,30 +17,34 @@ cl::opt<string> InputFilename(cl::Positional, cl::desc("<input file>"), cl::init
 // <input file> (required)
 // cl::opt<string> InputFilename(cl::Positional, cl::desc("<input file>"), cl::Required);
 
+
 int main(int argc, char **argv)
 {
     //   cl::ParseCommandLineOptions(argc, argv);
 
     cl::ParseCommandLineOptions(argc, argv, " A basic use of LLVM CommandLine \n\n"
                                             "  This program is an example of blah blah blah...\n");
+    bool InteractShellMode = HandleArgs(InputFilename, OutputFilename);
+    return 0;
+}
 
-   
-    if (InputFilename == "-")
+bool HandleArgs(const std::string& inFilename, const std::string& outFilename) {
+    bool  replMode = false;
+    if (inFilename == "-")
     {
         outs() << "Blank! No file name supplied.\nSwitching to Interactive Shell Mode.\n";
-        InteractShellMode = true;
-    }
-    
-    if (!InteractShellMode) {
-        std::ofstream Input(InputFilename.c_str());
-        if (Input.good())
-        {
-            outs() << "Yep, I can read the file " << InputFilename << " \n";
-        } else {
-            outs() << "Error: There is a problem reading the file " 
-                   << InputFilename << " ! \n";
-        }
+        replMode = true;
     }
 
-   return 0;
+    if (!replMode) {
+        ofstream Input(inFilename.c_str());
+        if (Input.good())
+        {
+            outs() << "Yep, I can read the file " << inFilename << " \n";
+        } else {
+            outs() << "Error: There is a problem reading the file "
+                   << inFilename << " ! \n";
+        }
+    }
+    return  replMode;
 }
